0f6ef4452a26c14fd1872235d61492da53293f42
TejaswiniPydipally <143117469+TejaswiniPydipally@users.noreply.github.com>
2024-03-04 21:20:39 -0600

    Added Home page

---
+++ Project/app/src/main/java/com/example/project/activity1.java
@@ -0,0 +1,21 @@
+package com.example.skywings;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class activity1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_1);
+    }
+
+    public void onClick(View v){
+        Intent ini = new Intent(this,activity2.class);
+        startActivity(ini);
+    }
+}
