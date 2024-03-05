ec15e1384a389de9f2e1e65c9cce5779b650115b
srikanthboppudi <srikanthboppudi4@gmail.com>
2024-03-04 21:50:46 -0600

    Layout2 and the Navigation code for the third Screen from Second screen

---
+++ SkyWings/app/src/main/java/com/example/skywings/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.skywings;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onClick(View v){
+        Intent ini = new Intent(this,activity1.class);
+        startActivity(ini);
+    }
+}
